a2a9a227239ebf3196820ca329d23081befe8b1c
JEMM6192 <julio15.edgardo1@gmail.com>
2025-04-07 21:55:21 -0600

    correciones

--- src/components/ClientPDFViewer.vue
+++ src/components/ClientPDFViewer.vue
@@ -1,7 +1,11 @@
 <!-- src/components/ClientPDFViewer.vue -->
 <template>
   <component :is="customEl" base-url="/TaniaPDF" id="pdfViewerElement">
-    <div id="viewerContainer" class="w-full h-full p-2 border border-base-200 rounded-lg bg-base-100 overflow-auto cursor-grab">
+    <!-- Contenedor del visor: permite scroll horizontal y vertical -->
+    <div
+      id="viewerContainer"
+      class="w-full h-full border border-base-200 rounded-lg bg-base-100 overflow-x-auto overflow-y-auto cursor-grab"
+    >
       <div class="relative">
         <canvas id="pdfCanvas" class="block"></canvas>
         <canvas id="overlayCanvas" class="absolute top-0 left-0 pointer-events-auto"></canvas>
